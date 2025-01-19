5bac5cfaeed135f97b33836f2f941a5d4012205c
saniyafarzindeveloper <developerfarzin@gmail.com>
2025-01-19 18:24:19 +0530

    added tiptap extensions

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,25 +1,48 @@
-'use client'
+"use client";
 
-import { useEditor, EditorContent } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Image from '@tiptap/extension-image'
 
 export default function Editor() {
   const editor = useEditor({
-    editorProps:{
-      attributes:{
+    editorProps: {
+      attributes: {
         style: "padding-left:56px; padding-right:56px",
-        class: "focus:outline-none print:border:0 bg-white border-[#C7C7C7] border flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
-      }
+        class:
+          "focus:outline-none print:border:0 bg-white border-[#C7C7C7] border flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
+      },
     },
-      extensions: [StarterKit],
-      content: '<p>Hello World! 🌎️</p>',
-    })
+    extensions: [
+      StarterKit,
+      Image,
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+      Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
+   
+    ],
+    content: '',
+  });
   return (
-    <div className='size-full overflow-x-auto bg-[#FBFDF9] px-4 print:p-0 print:bg-white print:overflow-visible'>
-      <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
-      <EditorContent editor={editor} />
+    <div className="size-full overflow-x-auto bg-[#FBFDF9] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
+        <EditorContent editor={editor} />
       </div>
-     
     </div>
-  )
+  );
 }
+
+
