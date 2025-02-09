e60e2177a762f5e1b2b1067ab326cf1b2689bb74
saniyafarzindeveloper <developerfarzin@gmail.com>
2025-02-09 19:42:55 +0530

    liveblocks initiated

---
+++ src/app/documents/[documentId]/room.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+export function Room({ children }: { children: ReactNode }) {
+  return (
+    <LiveblocksProvider publicApiKey={"pk_dev_Gwgqcr-klaRcmks1jjjCM5_7C-UT9j7ee4khSbmWSil3TZgtlm3uPaaRG0HrGpMv"}>
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
--- src/components/convex-client-provider.tsx
+++ src/components/convex-client-provider.tsx
@@ -32,3 +32,4 @@ export function ConvexClientProvider({ children }: { children: ReactNode }) {
     </ClerkProvider>
   );
 }
+
